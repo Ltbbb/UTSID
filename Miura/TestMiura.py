99294cdb7d10e88b08515e9a687d90bf6ac4092c
Lisa tb <lisatebraak@gmail.com>
2025-12-14 16:30:06 +0100

    restructure + relative imports working

--- Miura/TestMiura.py
+++ Miura/TestMiura.py
@@ -1,4 +1,15 @@
-import MaximumCurvature as mc
+import Miura.MaximumCurvature as mc
+from MATLAB.lee_region import lee_region
+import cv2 
+from PIL import Image
 
+imgpath = "test_imgs/1.png"
+img = cv2.imread(imgpath, 0)
+print(type(img))
+region, edges = lee_region(img, 4, 40)
 max_curvature = mc.MaximumCurvature()
-max_curvature("../MATLAB/demo_img_1.png")
+ret = max_curvature(img, region)
+
+print(ret)
+ret = Image.fromarray(ret)
+ret.show()
