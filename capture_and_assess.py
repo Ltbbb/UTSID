e0af12ffc74d257645382a926e8d6244a618f8fa
Lisa tb <lisatebraak@gmail.com>
2026-01-12 16:43:42 +0100

    refactoring to OOP structure

--- capture_and_assess.py
+++ capture_and_assess.py
@@ -1,75 +1,76 @@
-from picamera2 import Picamera2
+from utils.capture_framework import Framework
 import cv2
 import os
-import utils.capture_framework as cf
-import utils.preprocess as prep
+import numpy as np
 
-IMAGE_FOLDER = "img"
-img_ctr = 0
-
-#TODO: create image folder that self-cleans each round and stored end results only?
-image_paths = []  # List to store paths of captured images
-
-def main():
-    picam2 = Picamera2()
-    cf.init_cam(picam2)
-    cf.capture_images(picam2, capture_multiple)
-    best_img_f, best_img_l = determine_best_img()
-
-    #Display the best image
-    if best_img_f == best_img_l:
-        cf.display_img(best_img_f, "Best Image :)")
-    else:
-        cf.display_img(best_img_l, "Best Image Laplace")
-        cf.display_img(best_img_f, "Best Image Fourier")
-
-def capture_multiple(frame):
-    # Check for key press
-    key = cv2.waitKey(1)
-        
-    if key == ord(cf.KEY_TAKE_PICTURE):
-        global img_ctr
-        img_ctr += 1
-        if not os.path.exists(f"{IMAGE_FOLDER}"):
-            raise Exception(f"There is no folder called '{IMAGE_FOLDER}' in the current directory!")
-            #TODO: create folder for them?
-        #TODO: empty image folder?
-        image_name = f'{IMAGE_FOLDER}/image_{img_ctr}.jpg'
-        cv2.imwrite(image_name, frame)
-        image_paths.append(image_name)
-        print(f"Image captured and saved as '{image_name}'.")
-    elif key == ord(cf.KEY_QUIT):
-        cf.turn_off_leds()
-        raise Exception("Closing capture!") #FIXME: is there a better way to do this?
-
-def determine_best_img():
-     # Determine the best quality image using both Laplacian variance and Fourier sharpness
-    best_laplace_img = None
-    best_fourier_img = None
-
-    #init to 0
-    best_laplacian_variance = 0
-    best_fourier_sharpness = 0
-
-    for image_path in image_paths:
-        image = cv2.imread(image_path)
-        laplacian_var = prep.laplacian_variance(image)
-        fourier_sharp = prep.fourier_sharpness(image)
-        print(f"Image: {image_path}, Laplacian Variance: {laplacian_var}, Fourier Sharpness: {fourier_sharp}")
-
-        if laplacian_var > best_laplacian_variance:
-            best_laplacian_variance = laplacian_var
-            best_laplace_img = image_path
-
-        if fourier_sharp > best_fourier_sharpness:
-            best_fourier_sharpness = fourier_sharp
-            best_fourier_img = image_path
-
-    #TODO: combine metrics in a better way
-    print(f"Best quality fourier: '{best_fourier_img}' with value {best_fourier_sharpness}.")
-    print(f"Best quality laplace: '{best_laplace_img} with value {best_laplacian_variance}.") 
+class CaptureAssess(Framework): #inherit from framework
     
-    return best_fourier_img, best_laplace_img
+    def __init__(self):
+        self.IMAGE_FOLDER = "img"
+        self.img_ctr = 0
+        super().__init__() #init framework with capture multiple as callback
 
-#run main
-main()
+    def on_frame(self, frame):
+        # Check for key press
+        key = cv2.waitKey(1)
+            
+        if key == ord(Framework.KEY_TAKE_PICTURE):
+            self.img_ctr += 1
+            if not os.path.exists(f"{self.IMAGE_FOLDER}"):
+                raise Exception(f"There is no folder called '{self.IMAGE_FOLDER}' in the current directory!")
+                #TODO: create folder for them?
+            #TODO: empty image folder?
+            image_name = f'{self.IMAGE_FOLDER}/image_{self.img_ctr}.jpg'
+            cv2.imwrite(image_name, frame)
+            print(f"Image captured and saved as '{image_name}'.")
+        elif key == ord(Framework.KEY_QUIT):
+            Framework.turn_off_leds()
+            raise Exception("Closing capture!") #FIXME: is there a better way to do this?
+    
+    def determine_best_img(self):
+        # Determine the best quality image using both Laplacian variance and Fourier sharpness
+        best_laplace_img = None
+        best_fourier_img = None
+
+        #init to 0
+        best_laplacian_variance = 0
+        best_fourier_sharpness = 0
+
+        for image_path in os.listdir(self.IMAGE_FOLDER):
+            image = cv2.imread(image_path)
+            laplacian_var = self.laplacian_variance(image)
+            fourier_sharp = self.fourier_sharpness(image)
+            print(f"Image: {image_path}, Laplacian Variance: {laplacian_var}, Fourier Sharpness: {fourier_sharp}")
+
+            if laplacian_var > best_laplacian_variance:
+                best_laplacian_variance = laplacian_var
+                best_laplace_img = image_path
+
+            if fourier_sharp > best_fourier_sharpness:
+                best_fourier_sharpness = fourier_sharp
+                best_fourier_img = image_path
+
+        #TODO: combine metrics in a better way
+        print(f"Best quality fourier: '{best_fourier_img}' with value {best_fourier_sharpness}.")
+        print(f"Best quality laplace: '{best_laplace_img} with value {best_laplacian_variance}.") 
+        
+        return best_fourier_img, best_laplace_img
+    
+    # Function to calculate the Laplacian variance of an image
+    def laplacian_variance(self, image):
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        laplacian = cv2.Laplacian(gray, cv2.CV_64F)
+        variance = laplacian.var()
+        return variance
+
+    # Function to calculate the sum of the magnitude spectrum from the Fourier Transform
+    def fourier_sharpness(self, image):
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        dft = cv2.dft(np.float32(gray), flags=cv2.DFT_COMPLEX_OUTPUT)
+        dft_shift = np.fft.fftshift(dft)
+        magnitude_spectrum = cv2.magnitude(dft_shift[:, :, 0], dft_shift[:, :, 1])
+        sharpness = np.sum(magnitude_spectrum)
+        return sharpness
+    
+if __name__ == "__main__":
+    CaptureAssess()
--- capture_test_data.py
+++ capture_test_data.py
@@ -2,15 +2,31 @@ import cv2
 from picamera2 import Picamera2
 import time
 import utils.capture_framework as cf
+from enum import Enum
 
 KEY_NEXT_FINGER = "j"
 KEY_PREV_FINGER = "l"
 IMAGE_FOLDER = "img" 
 img_ctr = 0
 
-FINGER_IDX = 0
-FINGER = ["Right Index", "Right Middle", "Right Ring", "Left Index", "Left Middle", "Left Ring"]
-IMG_PATHS_CONTAINER = [[],[],[],[],[],[]] #NOTE! has to contain the same amt of listed lists as len(FINGER)
+# FINGER_IDX = 0
+# FINGER = ["L_Index", "L_Middle", "L_Ring", "R_Index", "R_Middle", "R_Ring"]
+# IMG_PATHS_CONTAINER = [[],[],[],[],[],[]] #NOTE! has to contain the same amt of listed lists as len(FINGER)
+
+class Finger(Enum):
+    L_INDEX = 1
+    L_MIDDLE = 2
+    L_RING = 3
+    R_INDEX = 4
+    R_MIDDLE = 5
+    R_RING = 6
+
+class Guide(Enum):
+    V1 = 1
+    V2 = 2
+    V3 = 3
+    V4 = 4
+    VU = 5
 
 def main():
     picam2 = Picamera2()
@@ -26,32 +42,35 @@ def main():
     #     display_img(best_img_f, "Best Image Fourier")
 
 def capture_test_data(frame):
-    global FINGER_IDX
+    global FINGER_IDX, FINGER, IMG_PATHS_CONTAINER, KEY_NEXT_FINGER, KEY_PREV_FINGER
 
-    current_finger_name = FINGER[FINGER_IDX]
+    current_guide_name = Guide(1)
+    current_finger_name = Finger(1)
     current_finger_array = IMG_PATHS_CONTAINER[FINGER_IDX]
 
-    cf.display_on_frame(f"Taking pictures of: {current_finger_name}finger")
+    cf.display_on_frame(f"Capturing {current_finger_name}")
 
-    key = cv2.waitKey(1)    
+    key = cv2.waitKey(1)   
 
-    if key == ord(cf.KEY_TAKE_PICTURE):
-        img_ctr = len(current_finger_array) + 1
-        image_name = f'{IMAGE_FOLDER}/{current_finger_name}_{img_ctr}.jpg'
-        cv2.imwrite(image_name, frame)
-        current_finger_array.append(image_name)
-        cf.display_on_frame(f"Image captured and saved as '{image_name}'.")
-        time.sleep(1) #TODO: delay to see img captured
-    elif key == ord(KEY_NEXT_FINGER):
-        if not FINGER_IDX >= len(FINGER) - 1:
-            FINGER_IDX += 1
-    elif key == ord(KEY_PREV_FINGER):
-        if not FINGER_IDX == 0:
-            FINGER_IDX -= 1
-    elif key == ord(cf.KEY_QUIT):
-        cf.turn_off_leds()
-        raise Exception("Closing capture!")
+    match key:
+        case ord(cf.KEY_TAKE_PICTURE):
+            img_ctr = len(current_finger_array) + 1
+            image_name = f'{IMAGE_FOLDER}/{current_finger_name}_{img_ctr}.jpg'
+            cv2.imwrite(image_name, frame)
+            current_finger_array.append(image_name)
+            cf.display_on_frame(f"Image captured and saved as '{image_name}'.")
+            time.sleep(1) #TODO: delay to see img captured
+        case ord(KEY_NEXT_FINGER):
+            if not FINGER_IDX >= len(FINGER) - 1:
+                FINGER_IDX += 1
+        case ord(KEY_PREV_FINGER):
+            if not FINGER_IDX == 0:
+                FINGER_IDX -= 1
+        case ord(cf.KEY_QUIT):
+            cf.turn_off_leds()
+            raise Exception("Closing capture!")
 
 #run main
-main()
+# main()
+print(Finger(1))
 
